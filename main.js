e90067de283e2d009bf7c0f458897715c9472ddc
Tom Hinkle (IACS) <thinkle@innovationcharter.org>
2025-03-17 13:52:46 +0000

    ++set name

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { TextInterface } from 'text-interface';
 // Create a new "Text Interface"
 let ti = new TextInterface();
 
-
+ti.setTitle("Hello World");
 let name = await ti.prompt("What is your name?");
 let age = await ti.promptNumber("How old are you?");
 ti.output("It is good to meet you, " + name + "!");
